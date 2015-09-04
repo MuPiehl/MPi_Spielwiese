d86be2d673567811eb3ff53775d7d7cf4ab30963
matthias.piehl <ma@wurst.de>
2015-09-04 15:56:36 +0200

    MPi-1 Init

---
+++ src/main/java/de/hypoport/omc/App.java
@@ -0,0 +1,13 @@
+package de.hypoport.omc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
